0eb92e7b76cff7dc7581404c1cafc00ad96cffb3
soila.torome <soila.torome@student.moringaschool.com>
2022-11-04 14:43:38 +0300

    Completed

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 6, 'F', "black", "calm", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("pam", 5, 'M', "white", "calm", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 13, 'F', "black", "playful", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Ginger", 4, 'M', "black", "calm", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Davis", 11, 'M', "brown", "calm", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 6, 'F', "black", "mild", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Luca", 5, 'M', "black", "calm", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (Null, 7, 'M', "black", "calm", 1);
